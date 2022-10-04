7a67b2c1ddbd7d9f40adcee0cd12a5652b1f6abd
Arnon <arnonrdp@gmail.com>
2022-10-04 19:55:22 -0300

    Basic configuration

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
+import '@quasar/extras/material-icons/material-icons.css'
+import { Quasar } from 'quasar'
+import 'quasar/src/css/index.sass'
+import { createApp } from 'vue'
+import './assets/main.css'
 
-import "./assets/main.css";
+import App from './App.vue'
+import router from './router'
 
-const app = createApp(App);
+const app = createApp(App)
 
-app.use(router);
+app.use(Quasar)
+app.use(router)
 
-app.mount("#app");
+app.mount('#app')
