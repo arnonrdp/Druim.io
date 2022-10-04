6a41c0f36a8c8d5bd65a72876a363362ea4a19fd
Arnon <arnonrdp@gmail.com>
2022-10-04 19:56:20 -0300

    Added Header and main pages

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <header class="flex">
+    <q-toolbar class="q-gutter-xl">
+      <RouterLink to="/">
+        <q-img alt="Druim logo" src="@/assets/logo.svg" width="10rem" />
+      </RouterLink>
+
+      <q-space />
+
+      <RouterLink to="/about">About us</RouterLink>
+      <RouterLink to="/services">Services</RouterLink>
+      <RouterLink to="/work">Work</RouterLink>
+      <RouterLink to="/contact">Contact us</RouterLink>
+    </q-toolbar>
+  </header>
+</template>
+
+<style scoped>
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,45 @@
-<script setup>
-import TheWelcome from "../components/TheWelcome.vue";
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="home">
+    <h1>EXCEPTIONAL PROFESSIONALS, BUILDING RELIABLE OPEN SOURCE SOLUTIONS.</h1>
+    <img src="@/assets/circle.svg" alt="" />
+  </div>
 </template>
+
+<style scoped>
+@media (min-width: 1024px) {
+  .home {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+
+h1 {
+  font-family: 'Goldman', Arial, sans-serif;
+  font-size: 3.25rem;
+  font-weight: 400;
+  line-height: 0.97;
+  margin: 11.25rem 0 0;
+  max-width: 400px;
+  position: relative;
+  text-transform: uppercase;
+  word-spacing: 0.2em;
+}
+
+img {
+  background: center center no-repeat;
+  background-size: contain;
+  left: 45%;
+  pointer-events: none;
+  position: absolute;
+  opacity: 0.655008;
+  top: -30%;
+  transform: translate(4.06802%, -0.0286551%) rotate(0deg) scale(0.928094);
+  -ms-transform-origin: 50% 50%;
+  transform-origin: 50% 50%;
+  transition-timing-function: linear;
+  transition-duration: 2000ms;
+  will-change: transform, opacity;
+  z-index: -1;
+}
+</style>
